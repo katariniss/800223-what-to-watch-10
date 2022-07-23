c7936734646ae4af9e29379b513e603daeac2a5f
Katariniss <echekina@gmail.com>
2022-07-23 18:07:46 +0500

    add video player

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,5 +1,7 @@
-import { Film } from '../../types/films';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Film } from '../../types/films';
+import VideoPlayer from '../video-player/video-player';
 
 type FilmCardProps = {
   film: Film;
@@ -9,18 +11,31 @@ type FilmCardProps = {
 function FilmCard(props: FilmCardProps): JSX.Element {
 
   const { film, mouseEnterHandler } = props;
+  const [isPreviewPlaying, setIsPreviewPlaying] = useState(false);
 
   return (
     <article className="small-film-card catalog__films-card"
-      onMouseOver={() => mouseEnterHandler(film)}
+      onMouseEnter={() => {
+        mouseEnterHandler(film);
+        setTimeout(() => {
+          setIsPreviewPlaying((prevState) => !prevState);
+        }, 1000);
+      }}
+      onMouseLeave={() => {
+        setIsPreviewPlaying(!isPreviewPlaying);
+      }}
     >
       <div className="small-film-card__image">
-        <img
-          src={film.posterImage}
-          alt={film.name}
-          width="280"
-          height="175"
-        />
+        {isPreviewPlaying ? (
+          <VideoPlayer player={film} />
+        ) : (
+          <img
+            src={film.posterImage}
+            alt={film.name}
+            width="280"
+            height="175"
+          />
+        )}
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>
