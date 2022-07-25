c7d42fa1e007d97ad28981771a55c62be932bc09
Katariniss <echekina@gmail.com>
2022-07-25 15:52:20 +0500

    get rid of disable

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useState } from 'react';
 
 function ReviewForm(): JSX.Element {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [userRating, setUserRating] = useState(0);
   const [userReview, setUserReview] = useState('');
 
@@ -18,7 +17,7 @@ function ReviewForm(): JSX.Element {
                     className="rating__input"
                     id={`star-${number}`}
                     type="radio" name="rating"
-                    value={number}
+                    value={userRating}
                     onChange={(evt) => setUserRating(Number(evt.target.value))}
                   />
                   <label
