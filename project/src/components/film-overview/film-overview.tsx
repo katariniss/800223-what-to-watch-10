ba9695e5d438048d074ae01bbf9c21f7bcb390cc
Katariniss <echekina@gmail.com>
2022-08-28 09:01:39 +0500

    calculate film mark

--- project/src/components/film-overview/film-overview.tsx
+++ project/src/components/film-overview/film-overview.tsx
@@ -5,14 +5,20 @@ type FilmOverviewProps = {
 }
 
 function FilmOverview({ film }: FilmOverviewProps): JSX.Element {
-  const { rating, mark, scoresCount, description, director, starring} = film;
+  const {
+    rating,
+    scoresCount,
+    description,
+    director,
+    starring,
+  } = film;
 
   return (
     <>
       <div className="film-rating">
         <div className="film-rating__score">{rating}</div>
         <p className="film-rating__meta">
-          <span className="film-rating__level">{mark}</span>
+          <span className="film-rating__level">{calculateMark(rating)}</span>
           <span className="film-rating__count">{`${scoresCount} ratings`}</span>
         </p>
       </div>
@@ -24,4 +30,19 @@ function FilmOverview({ film }: FilmOverviewProps): JSX.Element {
     </>
   );
 }
+
+function calculateMark(rating: number) {
+  if (rating < 3) {
+    return 'Bad';
+  } else if (rating >= 3 && rating < 5) {
+    return 'Normal';
+  } else if (rating >= 5 && rating < 8) {
+    return 'Good';
+  } else if (rating >= 8 && rating < 10) {
+    return 'Very Good';
+  } else {
+    return 'Awesome';
+  }
+}
+
 export default FilmOverview;
